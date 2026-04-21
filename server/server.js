634cb8c2e4eeb8bfbbeb6a4a9f37d0bc1cb0aef5
prajapatisakshi696-cmd <prajapatisakshi696@gmail.com>
2026-04-21 13:40:21 +0530

    Changes in server

--- server/server.js
+++ server/server.js
@@ -9,8 +9,22 @@ import postsRoutes from "./routes/posts.js";
 import followRoutes from "./routes/follow.js";
 
 dotenv.config();
-
 const app = express();
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://memories-xlaf-git-main-prajapatisakshi696-3250s-projects.vercel.app"
+];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
+
 
 // Middleware
 app.use(express.json());
